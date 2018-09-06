33c92abfc95c6d891697f6227ef153a4badbe055
庄希琦 <xiqiz@jumei.com>
2018-09-06 16:09:56 +0800

    Access-Control-Allow-Headers：*

--- server/index.js
+++ server/index.js
@@ -12,6 +12,7 @@ app.use(bodyParser.urlencoded({
 }));
 app.all('*', function (req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', '*');
     if (req.method == 'OPTIONS') res.send(200); /*让options请求快速返回*/
     else next();
 });
