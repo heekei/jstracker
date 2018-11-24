2acb69fa098a7599e6bcba9c419a4d823a7a14c4
Heekei <zxq0610@qq.com>
2018-11-24 16:02:11 +0800

    merged

--- client/jstracker.es6.js
+++ client/jstracker.es6.js
@@ -1,4 +1,4 @@
-// import axios from 'axios';
+import axios from 'axios';
 import postData from './postData'
 import md5 from 'js-md5';
 class jstracker {
@@ -180,44 +180,4 @@ class jstracker {
     };
   }
 }
-
-/**
- * 字符串转hash
- *
- * @param {*} str
- * @returns
- */
-// toHash(str) {
-//   str += '';
-//   var arr = new Array,
-//     len = str.length;
-//   var arg = Math.SQRT2.toFixed(9) - 0;
-//   forEach(function (x) {
-//     arr[x] = 0;
-//   });
-//   for (var i = 0; i < str.length; i++) calc(str.charCodeAt(i));
-//   forEach(function (x) {
-//     arr[x] = arr[x].toString(16);
-//     if (arr[x].length < 2) arr[x] = '0' + arr[x];
-//   });
-//   arr.reverse();
-//   return arr.join('');
-
-//   function calc(nmb) {
-//     var c = nmb & 255,
-//       next = nmb >> 8;
-//     forEach(function (x) {
-//       var h = (x ? arr[x - 1] : 0) + arr[x] + x + len + c;
-//       h += (h / arg).toFixed(9).slice(-3) - 0;
-//       arr[x] = h & 255;
-//     });
-//     if (next > 0) calc(next);
-//   }
-
-//   function forEach(func) {
-//     for (var i = 0; i < 16; i++) func(i);
-//   }
-// }
-// }
-
 export default jstracker;
